06b8a2a2488ba0706e7db60f9b158c15af7e87b2
ManuelFBS <manuelf.borrego@gmail.com>
2023-08-30 07:49:28 -0400

    Modificacion de ruta para app.js (Parte02_...)

---
+++ Parte02_Modulos_built-in/app.js
@@ -0,0 +1,12 @@
+/*
+  console.log(); || console.warn(); || console.error();
+  || console.assert(); || console.table();
+*/
+
+console.log('Hola a todos !!!');
+console.log();
+console.warn('Ocurrió un error...');
+console.log();
+console.error('Ocurrió un error...!');
+console.log();
+console.error(new Error('Ocurrió otro error...!'));
