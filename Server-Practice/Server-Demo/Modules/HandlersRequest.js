5c5715752b0206998a9dd74e2624bd85e1b3a3e4
ManuelFBS <manuelf.borrego@gmail.com>
2023-09-04 08:42:09 -0400

    Creac. de HandlersRequest.js y act. de app.js

---
+++ Server-Practice/Server-Demo/Modules/HandlersRequest.js
@@ -0,0 +1,72 @@
+const { infoCursos } = require('../Routing/cursos.js')
+
+const handleGetRequest = (req, res) => {
+  const path = req.url
+
+  console.log(res.statusCode)
+
+  switch (path) {
+    case '/':
+      return res.end(
+        'Bienvenido a mi 1er Servidor y API creados con Node.js !!!'
+      )
+    case '/api/cursos':
+      return res.end(JSON.stringify(infoCursos))
+    case '/api/cursos/programacion':
+      return res.end(JSON.stringify(infoCursos.programacion))
+    case '/api/cursos/matematicas':
+      return res.end(JSON.stringify(infoCursos.matematicas))
+    default:
+      res.statusCode = 404
+      return res.end('Error... No existe el recurso solicitado...!')
+  }
+}
+
+const handlePostRequest = (req, res) => {
+  const path = req.url
+
+  switch (path) {
+    case '/api/cursos/programacion':
+      return res.end(
+        'El servidor recibió una solicitud POST para "/api/cursos/programacion"'
+      )
+    default:
+      res.statusCode = 400
+      return res.end('Ha ocurrido un error al tratar de realizar el POST...!')
+  }
+}
+
+const handlePutRequest = (req, res) => {
+  const path = req.url
+
+  switch (path) {
+    case '/api/cursos/programacion':
+      return res.end(
+        'El servidor recibió una solicitud PUT para "/api/cursos/programacion"'
+      )
+    default:
+      res.statusCode = 400
+      return res.end('Ha ocurrido un error al tratar de realizar el PUT...!')
+  }
+}
+
+const handleDeleteRequest = (req, res) => {
+  const path = req.url
+
+  switch (path) {
+    case '/api/cursos/programacion':
+      return res.end(
+        'El servidor recibió una solicitud DELETE para "/api/cursos/programacion"'
+      )
+    default:
+      res.statusCode = 400
+      return res.end('Ha ocurrido un error al tratar de realizar el DELETE...!')
+  }
+}
+
+module.exports = {
+  handleGetRequest,
+  handlePostRequest,
+  handlePutRequest,
+  handleDeleteRequest
+}
