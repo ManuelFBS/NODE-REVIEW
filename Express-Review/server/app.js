efd94476313b4ae36cafba2642311934a0c686ea
ManuelFBS <manuelf.borrego@gmail.com>
2023-09-05 01:23:46 -0400

    Actual. de los package y .gitignore

--- Express-Review/server/app.js
+++ Express-Review/server/app.js
@@ -1,4 +1,14 @@
 const express = require('express')
+// const PORT = 3000
+const { PORT } = process.env
 const { infoCursos } = require('../api/cursos.js')
 
-const app = express()
+const server = express()
+
+server.get('/', (req, res) => {
+  res.send('Mi primer servidor... Cursos 💻.')
+})
+
+server.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`)
+})
